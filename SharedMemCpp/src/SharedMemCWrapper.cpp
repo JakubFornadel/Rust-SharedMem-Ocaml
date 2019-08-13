578cd13ac3d3ac297e735e9d381b4b842c53bf1e
Jakub Fornadel <jakub.fornadel@sophiatx.com>
2019-08-13 10:47:42 +0200

    Update SharedMemCWrapper.cpp

--- SharedMemCpp/src/SharedMemCWrapper.cpp
+++ SharedMemCpp/src/SharedMemCWrapper.cpp
@@ -4,22 +4,22 @@
 #include <SharedMem.h>
 
 struct shared_mem {
-	void *obj;
+  void *obj;
 };
 
 shared_mem_t* shared_mem_create(const char* file_name) {
-	shared_mem_t* m;
-	SharedMem*    obj;
+  shared_mem_t* m;
+  SharedMem*    obj;
 
-	m = (typeof(m))malloc(sizeof(*m));
+  m = (typeof(m))malloc(sizeof(*m));
   try { 
     obj = new SharedMem(file_name);
   } catch (...) {
-      std::cerr << "shared_mem_create exception thrown" << std::endl; 
+    std::cerr << "shared_mem_create exception thrown" << std::endl; 
   }
 
-	m->obj = obj;
-	return m;
+  m->obj = obj;
+  return m;
 }
 
 void shared_mem_destroy(shared_mem_t* m) {
@@ -39,11 +39,11 @@ int64_t shared_mem_sum(shared_mem_t* m, int64_t a, int64_t b) {
     return 0;
   }
 
-	obj = static_cast<SharedMem*>(m->obj);
-	try { 
+  obj = static_cast<SharedMem*>(m->obj);
+  try { 
     result = obj->sum(a, b);
   } catch (...) {
-      std::cerr << "shared_mem_sum exception thrown" << std::endl; 
+    std::cerr << "shared_mem_sum exception thrown" << std::endl; 
   }
     
   return result;
@@ -52,10 +52,10 @@ int64_t shared_mem_sum(shared_mem_t* m, int64_t a, int64_t b) {
 const char* shared_mem_file_name(shared_mem_t *m) {
   SharedMem *obj;
 
-	if (m == NULL) {
+  if (m == NULL) {
     return "";
   }
 
-	obj = static_cast<SharedMem*>(m->obj);
-	return obj->getFileName().c_str();
+  obj = static_cast<SharedMem*>(m->obj);
+  return obj->getFileName().c_str();
 }
